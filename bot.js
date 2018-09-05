f845cb1c756854d214754426f916776089acbf2a
gathome3 <42980060+gathome3@users.noreply.github.com>
2018-09-05 20:07:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,4 @@
-const Util = require('discord.js');
 
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-
-const prefix = "+";
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
